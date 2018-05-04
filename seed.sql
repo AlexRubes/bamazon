ad3754653db9c64eb5e60dd5cc77dd296b310f28
Alejandra Rubalcava <arubalcava@group.on>
2018-05-04 13:51:43 -0500

    seed

---
+++ seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+VALUES ('Fortnite', 'Video Games', 59.99, 360), 
+('HALO', 'Video Games', 29.98, 34),
+('Sour Patch Kids', 'Food & Drink', 4.29, 100),
+('Jorts', 'Apparel', 3.56, 45),
+('Mesh Shirt', 'Apparel', 45.67, 8),
+('Lion King', 'Movie', 19.99, 289),
+('Guess Who?', 'Board Games', 30.50, 345);
